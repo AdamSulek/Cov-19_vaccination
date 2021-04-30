6ea1ff0d150e0e31f528aa415199be5bfe950582
AdamSulek <48438721+AdamSulek@users.noreply.github.com>
2021-04-30 13:14:48 +0200

    Update settings.py
    as previous but with email password

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -156,6 +156,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'szczepieniaprzeciwcovid19@gmail.com'
-EMAIL_HOST_PASSWORD = config('secrets.EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'secrets.EMAIL_HOST_PASSWORD'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
