7750ca8a62468f9112c8ec839d1a532b37629923
AdamSulek <adam-s91@o2.pl>
2021-05-02 14:16:25 +0200

    commit

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: do't run with debug turned on in production!
-# DEBUG = config('DEBUG', cast=bool, default=True)
 DEBUG = config["DEBUG"]
 
 
