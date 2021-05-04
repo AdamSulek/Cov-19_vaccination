131022e6ed640decc23954ee3910ae4defe2db8f
AdamSulek <adam-s91@o2.pl>
2021-05-04 20:11:33 +0200

    looks stable on local server

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -174,8 +174,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
