f541a32401db2fd148e337ed77ab533cc8d26beb
AdamSulek <adam-s91@o2.pl>
2021-05-04 16:34:04 +0200

    nothing

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -169,8 +169,10 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+# EMAIL_HOST_USER = config.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'szczepieniaprzeciwcovid19@gmail.com'
+EMAIL_HOST_PASSWORD = 'Cov_19_12345'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
